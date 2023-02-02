00f796602277608f131b29dd2c50a0a8f99bb33c
Nakakandebr <bridgetnakakandea@gmail.com>
2023-02-02 22:22:57 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    var fullName = "Nakakande Bridget"
+    println(fullName)
+
+    var age = 23
+    println(age)
+
+    var phoneNumber = 757589859
+    println(phoneNumber)
+
+    var weightinkg: Byte = 60
+    println(weightinkg)
+
+    var citizen: Boolean = false
+    println(citizen)
+
+
+}
